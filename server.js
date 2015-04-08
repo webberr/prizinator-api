bf09eb6484682bf57f97f0bd7f67077641bb516f
Webber, Ryan (DS) <Ryan.Webber@cdk.com>
2015-04-07 17:50:48 -0700

    basic hapi example

---
+++ server.js
@@ -0,0 +1,33 @@
+var Hapi = require('hapi');
+var Meetup = require('meetup-api');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/event',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
